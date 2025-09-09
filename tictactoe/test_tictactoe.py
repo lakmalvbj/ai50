29be7725f46ebc275141567b1300d618d5154279
jlakmal@bournemouth.ac.uk <jlakmal@bournemouth.ac.uk>
2025-09-09 15:03:30 +0100

    Added minesweeper

--- tictactoe/test_tictactoe.py
+++ tictactoe/test_tictactoe.py
@@ -1,4 +1,5 @@
-from tictactoe import player, X, O, EMPTY, initial_state
+from tictactoe import player, X, O, EMPTY, initial_state, actions, result, winner
+from copy import deepcopy
 
 test_board= initial_state()
 
@@ -6,9 +7,49 @@ def test_player_at_the_start():
     assert player(test_board) == X
 
 def test_player_mid_stage():
-    test_board[0][1] =X
-    test_board[1][2]=O
-    test_board[1][0] = X
-    test_board[2][0] = O
-    print(test_board)
-    assert player(test_board) == X
+    current_board = deepcopy(test_board) 
+    current_board[0][1] =X
+    current_board[1][2]=O
+    current_board[1][0] = X 
+    assert player(current_board) == O
+
+def test_actions():    
+    assert actions(test_board) == [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
+
+def test_result():
+    current_board = [[X, EMPTY, EMPTY],
+            [EMPTY, O, X],
+            [EMPTY, EMPTY, EMPTY]]
+    assert result(current_board, (2,0)) == [[X, EMPTY, EMPTY],
+                                            [EMPTY, O, X],
+                                            [O, EMPTY, EMPTY]] 
+
+def test_winner_for_row_match():
+    current_board = [[X, EMPTY, EMPTY],
+            [O, O, O],
+            [EMPTY, EMPTY, EMPTY]]
+    assert winner(current_board) == O
+
+def test_winner_for_column_match():
+    current_board = [[X, EMPTY, EMPTY],
+            [X, O, O],
+            [X, EMPTY, EMPTY]]
+    assert winner(current_board) == X
+
+def test_winner_for_row_match():
+    current_board = [[X, EMPTY, EMPTY],
+            [O, X, O],
+            [EMPTY, EMPTY, X]]
+    assert winner(current_board) == X
+
+def test_winner_for_row_match():
+    current_board = [[X, EMPTY, O],
+            [O, O, O],
+            [O, EMPTY, EMPTY]]
+    assert winner(current_board) == O
+
+def test_winner_for_no_match():
+    current_board = [[X, EMPTY, EMPTY],
+            [O, EMPTY, O],
+            [EMPTY, EMPTY, EMPTY]]
+    assert winner(current_board) == None
--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+from copy import deepcopy 
 
 X = "X"
 O = "O"
@@ -36,39 +37,107 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    actions =list()
+    for i,x in enumerate(board):
+        for j,o in enumerate(x):
+            if(o == EMPTY):
+                actions.append((i,j))
+    return actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
-
+    current_player = player(board)
+    new_board =  deepcopy(board)
+    new_board[action[0]][action[1]] = current_player
+    return new_board
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
-    """
-    raise NotImplementedError
+    """ 
+    level = len(board)  
+    diagnol_top = []
+    diagnol_bottom = []
+    for i in range(level):  
+        diagnol_top.append(board[i][i]) 
+        diagnol_bottom.append(board[i][level - (i + 1)]) 
+        first_col = board[i][0]
+        column = ([row[i] for row in board])
+        first_row = column[0]
+        if(first_row != None and all(first_row == j for j in column)):
+            return first_row
+        if(first_col != None and all(first_col == j for j in board[i])):
+            return first_col
+    first_diagnol_top = diagnol_top[0]
+    if(first_diagnol_top != None and all(first_diagnol_top == j for j in diagnol_top)):
+        return first_diagnol_top
+    first_diagnol_bottom = diagnol_bottom[0]
+    if(first_diagnol_bottom != None and all(first_diagnol_bottom == j for j in diagnol_bottom)):
+        return first_diagnol_bottom 
 
+    return None
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
-
+    return winner(board) != None or len(actions(board)) == 0
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    winning_player = winner(board)
+    if winning_player == X:
+        return 1
+    if winning_player == O:
+        return -1
+    return 0
 
+def max_value(board):
+    if terminal(board):
+        return utility(board)
+    v = -math.inf
+    for action in actions(board):
+        v = max(v, min_value(result(board, action)))
+    return v
 
+def min_value(board):
+    if terminal(board):
+        return utility(board)
+    v = math.inf
+    for action in actions(board):
+        v = min(v, max_value(result(board, action)))
+    return v
+    
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
-    """
-    raise NotImplementedError
+    """     
+    if terminal(board):
+       return None
+    
+    next_action = None
+    current_player = player(board)
+    if current_player == X:
+        max = -math.inf
+        for action in actions(board):
+            print(action)
+            next_value = min_value(result(board, action))
+            if next_value > max:
+                max = next_value
+                next_action = action
+     
+    else:
+        min = math.inf
+        for action in actions(board):
+            next_value = max_value(result(board, action))
+            if next_value < min:
+                min = next_value 
+                next_action = action
+    
+    return next_action
+
